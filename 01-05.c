7957734cba264333d0c7768216c48dc79bb868c7
YYYEJI <22100396@handong.ac.kr>
2022-07-04 03:19:47 -0700

    Apartment Area

---
+++ 01-05.c
@@ -0,0 +1,15 @@
+// 아파트의 면적(제곱미터단위)을 평형으로 바꾸기
+#include <stdio.h>
+
+int main(void) {
+    float m2_area;       //면적 (제곱미터)
+    float pyung_area;    // 면적 (평수)
+
+    printf("m2_area? ");
+    scanf("%f", &m2_area);
+
+    pyung_area = m2_area/3.305;
+    printf("pyung_area is %.1f.", pyung_area);
+
+    return 0;
+}
