ae22c1aea71b9de51f927ef699a13be68f0ae922
YYYEJI <22100396@handong.ac.kr>
2022-07-04 04:35:22 -0700

    Find the Coordinates of a Quadratic Function

---
+++ 03-06.c
@@ -0,0 +1,20 @@
+// 2차 함수의 좌표 구하기
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;          // 2차 함수의 계수 a, b, c
+    int x_begin, x_end;   // x좌표의 시작 값과 끝 값
+    int x, y;             // x좌표, y좌표
+
+    printf("2차 함수의 계수 a와 b와 c를 입력 ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("x좌표의 시작 값과 끝 값을 입력 ");
+    scanf("%d %d", &x_begin, &x_end);
+   
+    for (x = x_begin ; x <= x_end; x++){
+        y = a*x*x+b*x+c;
+        printf("좌표 (%d,%d)\n", x, y);
+    }
+    return 0;
+}
