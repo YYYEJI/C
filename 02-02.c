ec4b17b38e69c0e5ebc49bc81e127409c1367a6c
YYYEJI <22100396@handong.ac.kr>
2022-07-04 03:44:57 -0700

    Obesity

---
+++ 02-02.c
@@ -0,0 +1,23 @@
+// 비만 판정
+#include <stdio.h>
+
+int main() {
+    int height, weight;    // 신장(cm), 체중(kg)
+    float bmi;             // 비만도 수치
+
+    printf("height? ");
+    scanf("%d", &height);
+
+    printf("weight? ");
+    scanf("%d", &weight);
+
+    bmi = weight/((height*0.01)*(height*0.01));
+
+    if(bmi>=25){
+        printf("You are overweight.");
+    }
+    else{
+        printf("You are not overweight.");
+    }
+    return 0;
+}
