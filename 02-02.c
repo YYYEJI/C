d674965709a91c66e85abee3345af194191d0403
YYYEJI <22100396@handong.ac.kr>
2022-07-04 20:19:55 -0700

    Obesity Check 1

--- 02-02.c
+++ 02-02.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    int height, weight;     // 신장(cm), 체중(kg)
-    float bmi;              // 비만도 수치
+    int height, weight;      // 신장(cm), 체중(kg)
+    float bmi;               // 비만도 수치
 
     printf("height? ");
     scanf("%d", &height);
