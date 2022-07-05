ddcd151b8d17a7935e0bd328ab232ea98203ba8c
YYYEJI <22100396@handong.ac.kr>
2022-07-04 20:01:35 -0700

    Triangle 2

--- 04-08.c
+++ 04-08.c
@@ -9,9 +9,9 @@ int main(void) {
     scanf("%d", &height);
 
     for(i = 0; i<height;i++){
-        for(j = 0; j <height-i-1; j++) printf(" ");
-        for(j = 0; j <= i; j++) printf("*");
-        for(j = 0; j <= i-1; j++) printf("*");
+        for(j = 0; j < height-i-1 ; j++) printf(" ");
+        for(j = 0; j <= i ; j++) printf("*");
+        for(j = 0; j <= i-1 ; j++) printf("*");
         printf("\n");
     }
     return 0;
