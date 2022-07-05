7232633697af04446f77efcb60672b3c0ba02ec2
YYYEJI <22100396@handong.ac.kr>
2022-07-04 20:00:48 -0700

    Square

--- 04-06.c
+++ 04-06.c
@@ -8,8 +8,8 @@ int main(void) {
     printf("정사각형의 크기를 입력하시오. ");
     scanf("%d", &length);
 
-    for(i = 0; i<length; i++){
-        for(j = 0; j<length; j++){
+    for(i = 0; i<length ; i++){
+        for(j = 0; j<length ; j++){
             printf("#");
         }
         printf("\n");
